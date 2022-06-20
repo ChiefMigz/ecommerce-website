47e2f43c7632436db13b55156d16b8fc053a070d
ChiefMigz <pebenitomiguel@gmail.com>
2022-06-19 21:37:46 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
