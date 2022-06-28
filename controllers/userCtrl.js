dffe5d4ce1c4835df0f53801374a8f8d118b4292
ChiefMigz <pebenitomiguel@gmail.com>
2022-06-27 22:35:14 -0700

    Implemented getUser

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -85,6 +85,15 @@ const userCtrl = {
         } catch (err) {
             return res.status(500).json({msg: error.message})
         }
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('-password');
+            if (!user) return res.status(400).json({msg: 'User does not exist.'})
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: error.message})
+        }
     }
 }
 
