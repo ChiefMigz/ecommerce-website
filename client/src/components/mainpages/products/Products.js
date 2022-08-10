73256b059be0610c4327a261635ec80f84b7baae
ChiefMigz <pebenitomiguel@gmail.com>
2022-08-10 10:49:09 -0700

    Updated design for product item, then implemented flexbox display on products

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,21 +1,25 @@
 import { useContext } from 'react';
 import { GlobalState }  from '../../../GlobalState';
 import ProductItem from '../utils/productitem/ProductItem';
+import './../../../index.css';
 
 const Products = () => {
     const state = useContext(GlobalState)
     
     const [products, setProducts] = state.productsAPI.products
 
-    return ( 
-        <div className='products'>
-        Product List:
+    return (
+        <div>
+            <h1>Always Available</h1>
+            <div className='cupcake-container'>
             {
                 products.map(product => {
                     return <ProductItem key={product._id} product={product}/>
                 })
             }
+            </div>
         </div>
+        
      );
 }
  
--- client/src/components/mainpages/utils/productitem/ProductItem.js
+++ client/src/components/mainpages/utils/productitem/ProductItem.js
@@ -1,12 +1,18 @@
+import '../../../../index.css';
+
 const ProductItem = ({product}) => {
     return ( 
-        <div>
-            <img src={product.images.url} alt=""/>
+        <div class="card">
 
-            <div className="product_box">
-                <h2 title={product.title}>{product.title}</h2>
-                <span>${product.price}</span>
-                <p>{product.description}</p>
+            <div class="imgBox">
+                <img src={product.images.url} alt="mouse corsair" class="mouse"/>
+            </div>
+
+            <div class="contentBox">
+                <h3>{product.name}</h3>
+                <small>Allergy Information</small>
+                <h2 class="price">{product.price} CAD</h2>
+                <a href="#" class="buy">Add to Cart</a>
             </div>
         </div>
      );
