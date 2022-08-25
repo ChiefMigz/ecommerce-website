aa5fb3538683c32aa3f86eacd974031b4657c76a
ChiefMigz <pebenitomiguel@gmail.com>
2022-08-25 11:57:06 -0700

    Minor convention fix

--- client/src/components/mainpages/utils/productitem/ProductItem.js
+++ client/src/components/mainpages/utils/productitem/ProductItem.js
@@ -4,24 +4,24 @@ import {FaCartPlus} from 'react-icons/fa';
 
 const ProductItem = ({product}) => {
     return ( 
-        <div class="card">
+        <div className='card'>
             <Link to={`/productInfo/${product._id}`}>
-                <div class="imgBox">
-                    <img src={product.images.url} alt={product.name} class="product"/>
+                <div className='imgBox'>
+                    <img src={product.images.url} alt={product.name} class='product'/>
                 </div>
 
-                <div class="contentBox">
+                <div className='contentBox'>
                     <small>Allergy Information</small>
                     <h3>{product.name}</h3>
-                    <h2 class="price">$ {product.price}</h2>
-                    <div class='categories'>
+                    <h2 class='price'>$ {product.price}</h2>
+                    <div className='categories'>
                         {
                             product.categories.map(cateogry => {
                                 return <span id='category-item'>{cateogry}</span>
                             })
                         }
                     </div>
-                    <button class="buy">Add to Cart<FaCartPlus class='add-cart-icon' size={20}/></button>
+                    <button class='buy'>Add to Cart<FaCartPlus class='add-cart-icon' size={20}/></button>
                 </div>
             </Link>
         </div>
