c23868f3a1f8f902ce1210a2e2f6aec610b91310
ChiefMigz <pebenitomiguel@gmail.com>
2022-09-02 11:03:21 -0700

    Refactored ProductItem js and css

--- client/src/components/mainpages/utils/productitem/ProductItem.js
+++ client/src/components/mainpages/utils/productitem/ProductItem.js
@@ -1,31 +1,58 @@
-import '../../../../index.css';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {FaCartPlus} from 'react-icons/fa';
+import '../../../../index.css';
+import NotificationModal from '../modals/notification/NotificationModal';
 
-const ProductItem = ({product}) => {
-    return ( 
+class ProductItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props,
+            allergies: {
+                title: 'test',
+                description: 'description'
+            },
+            active: false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        const query = e.target.id;
+        this.props.product.forEach(product=> {
+            if (product._id === query)
+                console.log(product.name) 
+        })
+    }
+    
+    render() { 
+        const {state: {product}} = this; 
+        return ( 
         <div className='card'>
             <Link to={`/productInfo/${product._id}`}>
                 <div className='imgBox'>
-                    <img src={product.images.url} alt={product.name} class='product'/>
+                    <img src={product.images.url} alt={product.name} className='product'/>
                 </div>
-
-                <div className='contentBox'>
-                    <small>Allergy Information</small>
-                    <h3>{product.name}</h3>
-                    <h2 class='price'>$ {product.price}</h2>
-                    <div className='categories'>
-                        {
-                            product.categories.map(cateogry => {
-                                return <span id='category-item'>{cateogry}</span>
-                            })
-                        }
-                    </div>
-                    <button class='buy'>Add to Cart<FaCartPlus class='add-cart-icon' size={20}/></button>
-                </div>
+                    <div className='detailLink'>&nbsp;</div>
             </Link>
+                
+            <div className='contentBox' onClick={(e)=>e.preventDefault()}>
+                <span className='allergyInfo' id={product._id} onClick={this.handleClick}>Allergy Information</span>    
+                <h3>{product.name}</h3>
+                <h2 className='price'>$ {product.price}</h2>
+                <div className='categories'>
+                    {
+                        product.categories.map(cateogry => {
+                            return <span id='category-item'>{cateogry}</span>
+                        })
+                    }
+                </div>
+                <button className='buy'>Add to Cart<FaCartPlus className='add-cart-icon' size={20}/></button>
+            </div>
+            
         </div>
-     );
+        );
+    }
 }
- 
+
 export default ProductItem;
